b67e2d37ffd05172d29f4cc1c249b25469886f4e
PhilippReiterer <65352037+PhilippReiterer@users.noreply.github.com>
2020-05-20 17:04:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var path = require('path')
 var app = express()
 app.set('port', (process.env.PORT || 5000))
 
-app.use(express.static(__dirname + 'public', 'ceasar_Test.html')
+var dateipfad = path.join(__dirname, 'public', 'ceasar_Test.html')
 
 app.get('/', function(request, response) {
   //response.send('Hello World!')
