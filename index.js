e001fd67819ea086a1620c00f88dfe3de2eb191b
PhilippReiterer <65352037+PhilippReiterer@users.noreply.github.com>
2020-05-20 16:56:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express')
-var app = express()
+var app = express('path')
 
+var app = express()
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+
+app.use(express.static(__dirname + '/public', + 'ceasarTest.html'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  //response.send('Hello World!')
+  response.sendFile(dateipfad)
 })
 
 app.listen(app.get('port'), function() {
